fe9e73862dbdaa895c092d03256cadbff05aa8ac
Matt <matthewspoche@comcast.net>
2020-06-14 00:54:33 -0500

    changed titlebar color

--- config.h
+++ config.h
@@ -13,11 +13,12 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_burgundy[] 	= "#800020";
 static const char col_white[]		= "#ffffff";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_white },
+	/*               fg         bg         	   border   */
+	[SchemeNorm] = { col_gray3, col_gray1, 	   col_gray2 },
+	[SchemeSel]  = { col_gray4, col_burgundy,  col_white },
 };
 
 /* tagging */
